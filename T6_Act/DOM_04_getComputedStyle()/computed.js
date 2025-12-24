c736f0e00502000b59df7d6e84ab3f9d07943c80
Rafael_Cosquillo <cosquillo00@gmail.com>
2025-12-24 07:33:32 +0100

    getcomputed completo

---
+++ T6_Act/DOM_04_getComputedStyle()/computed.js
@@ -0,0 +1,4 @@
+const caja = document.querySelector('#caja');
+console.log(getComputedStyle(caja).width);
+console.log(getComputedStyle(caja).height);
+console.log(getComputedStyle(caja).background);
