7d1cee03d22292af958d42904d526c6bf896d37f
Rafael Cosquillo <cosquillo00@gmail.com>
2026-01-14 12:28:04 +0100

    dom_parte_2 hasta el 7

--- DOM_parte_2/1_RenderEstado/gameState.js
+++ DOM_parte_2/1_RenderEstado/gameState.js
@@ -1,3 +1,5 @@
+
+
 const gameState = {
     pawns: ["♙", "a", "♙", "♙", "♙", "♙", "b", "♙"]
 }
@@ -57,6 +59,7 @@ function crearSquare(fila, col){
     asignarColor(square, fila, col);
     square.id = columnas[col] + fila;
     console.log(square.id);
+    square.dataset.pos = square.id;
 
     return square;
 }
@@ -74,9 +77,65 @@ function crearBoard(){
 }
 crearBoard()
 //----5----
+/*
 const celdas = document.querySelectorAll('.square');
 celdas.forEach(celda => {
     celda.addEventListener('click', () => {
         alert('Celda: ' + celda.id);
     });
+});*/
+//----6----
+const gameState2 = {
+    pieces: [{type: "pawn", pos:"a2"},
+        {type: "pawn", pos:"b2"},
+        {type: "pawn", pos:"c2"},
+        {type: "pawn", pos:"d2"},
+        {type: "pawn", pos:"e2"},
+        {type: "pawn", pos:"f2"},
+        {type: "pawn", pos:"g2"},
+        {type: "pawn", pos:"h2"}]
+}
+function renderPieces(state) {
+    state.pieces.forEach(piece => {
+        const squarePeon = board.querySelector(`[data-pos='${piece.pos}']`);
+        if (squarePeon) {
+            squarePeon.textContent = "♙";
+        }
+    })
+}
+renderPieces(gameState2);
+//----7----
+const celdasPeon = document.querySelectorAll('.square');
+celdasPeon.forEach(celda => {
+    celda.addEventListener('click', () => {
+        if(celda.textContent === "♙") {
+            limpiarHighlights()
+            //Resaltar posibles movimientos
+            let celdaPosteriorId = celda.id[0] + (parseInt(celda.id[1]) + 1);
+            const posiblesMovs = [celdaPosteriorId];
+            if ((parseInt(celda.id[1])) === 2) {
+                posiblesMovs.push(celda.id[0] + (parseInt(celda.id[1]) + 2));
+            }
+            posiblesMovs.forEach(posible => {
+                const celdaPosible = board.querySelector(`[data-pos='${posible}']`);
+                celdaPosible.classList.toggle('highlight');
+                //Mover peon a casilla resaltada
+                celdaPosible.addEventListener('click', () => {
+                    if(celdaPosible.classList.contains('highlight')){
+                        celdaPosible.textContent = "♙";
+                        celda.textContent = "";
+                        limpiarHighlights();
+                    }
+                })
+            })
+
+        }
+    })
 });
+
+function limpiarHighlights() {
+    const celdas = document.querySelectorAll('.square');
+    celdas.forEach(celda => {
+        celda.classList.remove('highlight');
+    });
+}
